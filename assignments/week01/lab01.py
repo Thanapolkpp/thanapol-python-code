49eef37c631df3da0df54ec9a93da3735f7436a7
Thanapol Khampimpit <thanapol.kha@ku.th>
2025-07-08 11:59:46 +0700

    ad new file week01

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,17 @@
+
+print("=== Personal Profile Creator ===")
+fullname = input("What is your name: ")
+age = input("How old are your:")
+age = int(age)
+email = input("What is your email: ")
+phone = input("What is your phone number:")
+hobby = input("what i your favorite hobby:")
+
+
+print("==Your profile==")
+print("Full name is",fullname)
+print("Age is",age)
+print("Email is",email)
+print("phone number:",phone)
+print("favorite hobby:",hobby)
+
